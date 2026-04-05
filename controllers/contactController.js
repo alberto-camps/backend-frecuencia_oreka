58de411e859792555546f8cf08be040f402080aa
Alberto <campsbass@gmail.com>
2026-04-05 20:03:09 +0200

    Cambiado res.send

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,11 +9,12 @@ const contactController = {
             // Guardar en MongoDB
             const newContact = new ContactModel({ name, email, phone, message });
             await newContact.save();
+            
+            res.status(201).json({ message: 'Mensaje enviado correctamente' });
 
             //Envíar al gmail
             await sendEmail({ name, email, phone, message });
 
-            res.status(201).json({ message: 'Mensaje enviado correctamente' });
         } catch (error) {
             console.error('Error al crear el contacto:', error);
             res.status(500).json({ message: 'Error al procesar el mensaje' });
